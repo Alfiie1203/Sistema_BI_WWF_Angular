70133296335e793ba41d39861ce4e952c63844cf
Luis Alfredo Rangel <luis.rangel@proforest.net>
2023-01-27 15:24:32 -0500

    1

--- src/app/component/cuestionario/cuestionario.component.ts
+++ src/app/component/cuestionario/cuestionario.component.ts
@@ -1,4 +1,4 @@
-import { HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Kobo_ApiServiceService } from 'src/app/service/kobo_api-service.service';
 import { ServiceKoboService } from 'src/app/service/service-kobo.service';
@@ -11,19 +11,20 @@ import { ServiceKoboService } from 'src/app/service/service-kobo.service';
 export class CuestionarioComponent implements OnInit {
   public today = new Date();
 
-  constructor(private service: ServiceKoboService) { }
+  constructor(private service: ServiceKoboService, private http: HttpClient) { }
   id = 211787093;
   array = [];
 
-  public headers_object = new HttpHeaders;
 
 
   ngOnInit(): void {
     this.getData()
-    this.headers_object.append('Authorization','Token 5a70d9a7db0b528c4884b11a5ed21a1eab15409e')
+
   }
 
   public getData(){
+
+
     this.service.getInformacionById().subscribe(respuesta => {
       for (let index = 0; index < respuesta.length; index++) {
         const element = respuesta[index];
@@ -34,6 +35,7 @@ export class CuestionarioComponent implements OnInit {
       }      
     })
   }
+
 
 
 
